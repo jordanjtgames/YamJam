7461b4c8af0e9fcf1e672a958e84e854f56f0c8a
JayParis20 <72521841+JayParis20@users.noreply.github.com>
2023-01-24 13:56:16 +0000

    Shrink Start

--- Assets/Scripts/ClimbingPoI.cs
+++ Assets/Scripts/ClimbingPoI.cs
@@ -12,22 +12,57 @@ public class ClimbingPoI : MonoBehaviour
 
     public Renderer myRend;
 
+    Vector3 startLocalScale;
+    bool holdingPlayer = false;
+    float holdingCheck = 0.1f;
+
+    //float shrink_t = 0;
+
     void Awake()
     {
-        if(myRend == null)
-            myRend = GetComponent<Renderer>();
+        if (id == 1) {
+            if (myRend == null)
+                myRend = GetComponent<Renderer>();
+        } else if (id == 2) {
+            startLocalScale = transform.localScale;
+            if (myRend == null)
+                myRend = transform.parent.GetChild(1).GetComponent<Renderer>();
+        }
+
+        
     }
 
     void Update()
     {
+        if(id == 1) {
+
+        } else if(id == 2) {
+
+        }
+
         if (highlighted) {
             checkDelay -= Time.deltaTime;
 
             if(checkDelay <= 0) {
                 highlighted = false;
             }
+            
         } else {
             hightlight = Mathf.Lerp(hightlight, 0f, Time.deltaTime * 8f);
+        }
+
+        if (holdingPlayer) {
+            holdingCheck -= Time.deltaTime;
+            if(holdingCheck <= 0) {
+                holdingPlayer = false;
+            }
+        }
+
+        if (id == 2) {
+            transform.localScale = Vector3.MoveTowards(transform.localScale, startLocalScale * (holdingPlayer ? 0.01f : 1f), Time.deltaTime * 6f);
+            if(holdingPlayer && transform.localScale.x < 0.5f) {
+                Debug.LogError("KickPlayer");
+            }
         }
 
         if (myRend != null)
@@ -39,5 +74,10 @@ public class ClimbingPoI : MonoBehaviour
         highlighted = true;
         checkDelay = 0.15f;
         Debug.Log("Highlih");
+    }
+
+    public void HoldingPlayer() {
+        holdingCheck = 0.15f;
+        holdingPlayer = true;
     }
 }
